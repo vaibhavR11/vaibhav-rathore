a27fb34deddeae7722e85936d8a4e7f1ebe84b7a
Vaibhav Rathore <69416365+vaibhavR11@users.noreply.github.com>
2020-08-10 08:50:55 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,11 @@
+# check whether the number is odd or even, without using funtion calling
+
+num = int(input("Enter a number :"))
+if num>=1 :
+  if num % 2 == 0 :
+     print("{} is even :".format(num))
+  else :
+     print("{} is odd :".format(num))
+else :
+	print("{} is neither even nor odd".format(num))
+   
